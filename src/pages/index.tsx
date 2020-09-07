106d9a22131050ad6a532fe8308f4b420472fbe1
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-09-07 19:21:43 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = "React Avançado" }: Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>main</main>
+
+      <footer>footer</footer>
+    </div>
+  );
+}
