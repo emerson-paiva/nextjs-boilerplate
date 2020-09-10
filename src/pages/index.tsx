960d568688853d3e57655642a3e3b99b2e1ccfe6
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-09-09 22:19:39 -0300

    feat: add default next components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,7 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
-type Props = {
-  title: string
+function Home() {
+  return <Main />
 }
 
-export default function Home({ title = 'React Avançado' }: Props) {
-  return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>main</main>
-
-      <footer>footer</footer>
-    </div>
-  )
-}
+export default Home
